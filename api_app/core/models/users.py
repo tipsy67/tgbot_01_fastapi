56e2c851e19f46a2ae46c866765cd91265c81b26
dan.toropov(home) <dan.toropov@gmail.com>
2025-09-06 00:30:14 +0300

    create models

--- api_app/core/models/users.py
+++ api_app/core/models/users.py
@@ -1,12 +1,28 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from typing import Optional
 
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.sql.sqltypes import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import String, CHAR, DateTime, Integer
 
 from api_app.core.config import settings
 
+
+class TicketAction:
+    REFERRAL = 'rfrl'
+    BUYER = 'buyr'
+
+    DISPLAY_NAMES = {
+        REFERRAL: 'Приглашение',
+        BUYER: 'Покупка',
+    }
+
+    ALL_STATUS = [REFERRAL, BUYER,]
+
+    @classmethod
+    def get_display_name(cls, ticked_action):
+        return cls.DISPLAY_NAMES.get(ticked_action, 'Неизвестно')
 
 class Base(DeclarativeBase):
     pass
@@ -25,12 +41,46 @@ class User(Base):
     last_activity: Mapped[datetime] = mapped_column()
     is_staff: Mapped[bool] = mapped_column(default=False)
     is_admin: Mapped[bool] = mapped_column(default=False)
-    ticket_id: Mapped[Optional[str]] = mapped_column()
+    tickets: Mapped[list["Ticket"]] = relationship(
+        "Ticket",
+        back_populates="user",
+        cascade="all, delete-orphan",
+        foreign_keys="Ticket.user_id"
+    )
+    initiated_tickets: Mapped[list["Ticket"]] = relationship(
+        "Ticket",
+        back_populates="initiator",
+        foreign_keys="Ticket.initiator_id"
+    )
 
 
 class Ticket(Base):
     __tablename__ = "tickets"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
+    action: Mapped[CHAR] = mapped_column(CHAR(4))
+    action_description: Mapped[str] = mapped_column(String(255), default="", nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    initiator_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+
+    user: Mapped["User"] = relationship("User", back_populates="tickets", foreign_keys=[user_id])
+    initiator: Mapped["User"] = relationship("User", back_populates="initiated_tickets", foreign_keys=[initiator_id])
+
     is_fired: Mapped[bool] = mapped_column(default=False)
-    created_at: Mapped[datetime] = mapped_column()
-    action: Mapped[Optional[str]] = mapped_column(default=None)
+    fired_at: Mapped[datetime] = mapped_column(DateTime)
+    prize_id = Mapped[int] = mapped_column(Integer, ForeignKey("prizes.id"), nullable=False)
+    ticket = relationship(
+        "Prize",
+        back_populates="prizes"
+    )
+
+class Prize(Base):
+    __tablename__ = "prizes"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime)
+    is_active: Mapped[bool] = mapped_column(default=False)
+    description: Mapped[str] = mapped_column(String(255), default="")
+    tickets = relationship(
+        "Ticket",
+        back_populates="tickets",
+    )
